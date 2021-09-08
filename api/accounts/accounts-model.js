46cecaf968313f5a01f7116a270e45ac30889fe0
Rodgers31 <otienor986@gmail.com>
2021-09-07 20:25:34 -0400

    all tests pass

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,35 +1,27 @@
 const db = require('../../data/db-config');
 
 const getAll = () => {
+	// select * from accounts;
 	return db('accounts');
 };
 
 const getById = (id) => {
+	//select 7 from accounts whre id =2
 	return db('accounts').where('id', id).first();
 };
 
 const create = async (account) => {
-	const [id] = await db('accounts').insert({ account });
-	const newAccount = await getById(id);
-	return newAccount;
+	const [id] = await db('accounts').insert(account);
+	return getById(id);
 };
 
 const updateById = async (id, account) => {
-	return db('accounts')
-		.where('id', id)
-		.update({ account })
-		.then(() => {
-			return getById(id);
-		});
+	await db('accounts').where('id', id).update(account);
+	return getById(id);
 };
 
 const deleteById = async (id) => {
-	return db('accounts')
-		.where('id', id)
-		.del()
-		.then(() => {
-			return getById(id);
-		});
+	return db('accounts').where('id', id).del();
 };
 
 module.exports = {
